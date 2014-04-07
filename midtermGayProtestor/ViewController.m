feb30ddb13904e1a7df1ae433f2f6495c94b7266
siuhim <siuhim.tang@gmail.com>
2014-04-07 17:00:01 -0400

    1st version

--- midtermGayProtestor/ViewController.m
+++ midtermGayProtestor/ViewController.m
@@ -14,18 +14,25 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+}
 
+- (void)viewWillLayoutSubviews
+{
+    [super viewWillLayoutSubviews];
     // Configure the view.
-    SKView * skView = (SKView *)self.view;
-    skView.showsFPS = YES;
-    skView.showsNodeCount = YES;
-    
-    // Create and configure the scene.
-    SKScene * scene = [MyScene sceneWithSize:skView.bounds.size];
-    scene.scaleMode = SKSceneScaleModeAspectFill;
-    
-    // Present the scene.
-    [skView presentScene:scene];
+    // Configure the view after it has been sized for the correct orientation.
+    SKView *skView = (SKView *)self.view;
+    if (!skView.scene) {
+        skView.showsFPS = NO;
+        skView.showsNodeCount = NO;
+        
+        // Create and configure the scene.
+        MyScene *theScene = [MyScene sceneWithSize:skView.bounds.size];
+        theScene.scaleMode = SKSceneScaleModeAspectFill;
+        
+        // Present the scene.
+        [skView presentScene:theScene];
+    }
 }
 
 - (BOOL)shouldAutorotate
